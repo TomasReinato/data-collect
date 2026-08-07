b5ab8bb94e787573d5df1425b0af4aac826b3109
TomasReinato <tomasl.reinato@outlook.com>
2026-08-07 12:16:50 -0300

    salvando

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -1,8 +1,20 @@
 # %%
 import requests
+from bs4 import BeautifulSoup
 
-url ="https://www.google.com"
+url ="https://www.residentevildatabase.com/personagens/ada-wong/"
 
 resp = requests.get(url)
 # %%
 resp.status_code
+# %%
+resp.text
+# %%
+soup = BeautifulSoup(resp.text)
+soup
+# %%
+div_page = soup.find("div", class_ = "td-page-content")
+div_page
+# %%
+div_page.find_all("p")[1]
+# %%
