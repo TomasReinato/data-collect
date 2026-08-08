1759dda929b9df389625d17d8840e3603482426a
TomasReinato <tomasl.reinato@outlook.com>
2026-08-07 22:48:44 -0300

    add basic infos

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -2,19 +2,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-url ="https://www.residentevildatabase.com/personagens/ada-wong/"
+def get_content(url):
+    resp = requests.get(url)
+    return resp
 
-resp = requests.get(url)
-# %%
-resp.status_code
-# %%
-resp.text
-# %%
+def get_basic_infos(soup):
+    div_page = soup.find("div", class_ = "td-page-content")
+    paragrafo = div_page.find_all("p")[1]
+    ems = paragrafo.find_all("em")
+    data = {}
+    for i in ems:
+        chave,  valor = i.text.split(":")
+        chave = chave.strip(" ")
+        data[chave] = valor
+
+    return data
+
+url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
+resp = get_content(url)
+
+if resp.status_code != 200:
+    print('Nãop foi possível obter os dados')
+
 soup = BeautifulSoup(resp.text)
 soup
-# %%
-div_page = soup.find("div", class_ = "td-page-content")
-div_page
-# %%
-div_page.find_all("p")[1]
+
+get_basic_infos(soup)
+data
+
 # %%
