a7203c888aac326867be5172e7681d48c9c416b0
TomasReinato <tomasl.reinato@outlook.com>
2026-08-07 23:33:54 -0300

    fazendo a coleta das urls dos personagens

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -1,7 +1,11 @@
 # %%
+###IMPORTS
+
 import requests
 from bs4 import BeautifulSoup
 
+# %%
+###FUNCTIONS
 def get_content(url):
     resp = requests.get(url)
     return resp
@@ -18,16 +22,44 @@ def get_basic_infos(soup):
 
     return data
 
-url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
+def get_apperances(soup):
+    lis = (soup.find("div", class_ = "td-page-content")
+       .find('h4')
+       .find_next()
+       .find_all('li')
+    )
+    appearances = [i.text for i in lis]
+    return appearances
+
+
+# %%
+url = get_url
 resp = get_content(url)
 
 if resp.status_code != 200:
     print('Nãop foi possível obter os dados')
+else:
+    soup = BeautifulSoup(resp.text)
+    data = get_basic_infos(soup)
+    data['Appearances'] = get_apperances(soup)
 
-soup = BeautifulSoup(resp.text)
-soup
-
-get_basic_infos(soup)
 data
+# %%
+url_personagens = 'https://www.residentevildatabase.com/personagens/'
+personagens = get_content(url_personagens)
+if personagens.status_code != 200:
+    print('Nãop foi possível obter os dados')
+else:
+    print(personagens.status_code)
+soup_personagens = BeautifulSoup(personagens.text)
+get_url = (
+    soup_personagens
+    .find("div", class_ = "td-page-content")
+    .find('h3')
+    .find_next('a')
+    .get('href')
+)
+get_url
+
 
 # %%
